4dfd3a1ac3ad190a47e802a41ec5e8cf9d959bba
elor1 <57156027+elor1@users.noreply.github.com>
2021-06-12 03:06:49 +0100

    Added tether to player
    Imported ship & player sprites
    Added tether to connect player to the ship

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float distanceFromTetherEnd = 0.3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ConnectTether(Rigidbody2D tetherEnd)
+    {
+        HingeJoint2D joint = gameObject.AddComponent<HingeJoint2D>();
+        joint.autoConfigureConnectedAnchor = false;
+        joint.connectedBody = tetherEnd;
+        joint.anchor = Vector2.zero;
+        joint.connectedAnchor = new Vector2(0.0f, distanceFromTetherEnd);
+    }
+}
